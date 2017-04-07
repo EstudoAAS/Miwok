8e8e72f655c526abd27d0478ff9eacddd0b30b3a
Aller Augusto de Souza <allerasouza@gmail.com>
2017-04-06 23:28:07 -0300

    Set background color for activities

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,14 +1,17 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,9 +21,11 @@ import java.util.ArrayList;
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
+    private int mColorResourceId;
 
-    public WordAdapter(Context c, ArrayList<Word> w){
-        super(c, 0, w);
+    public WordAdapter(Context context, ArrayList<Word> words, int colorResourceId){
+        super(context, 0, words);
+        this.mColorResourceId = colorResourceId;
     }
     @NonNull
     @Override
@@ -43,6 +48,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
             imageView.setVisibility(View.VISIBLE);
         } else
             imageView.setVisibility(View.GONE);
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        textContainer.setBackgroundColor(ContextCompat.getColor(this.getContext(),mColorResourceId));
 
         return listItemView;
     }
